6697c277a41468cad954ef5c60c81aa5b22c9cdd
Abdul Wajid <83264143+wajidjamali@users.noreply.github.com>
2025-04-18 14:55:26 +0500

    Update and rename check-balance.ts to labs/check-balance.ts

--- labs/check-balance.ts
+++ labs/check-balance.ts
@@ -1,11 +1,11 @@
 import { Connection, PublicKey, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js"
 const publickey = "6EDB91UvJrmErTF81rmJFcS22mdn6eYiSJG5EASXpnDS"
 
-// const suppliedPublicKey = process.argv[2]
-// if (!suppliedPublicKey){
-//     throw new Error("Provide a public key to check the balance of!");
+ const suppliedPublicKey = process.argv[2]
+ if (!suppliedPublicKey){
+     throw new Error("Provide a public key to check the balance of!");
     
-// }
+ }
 
 const connection = new Connection("https://api.mainnet-beta.solana.com", "confirmed")
 const address = new PublicKey(publickey)
